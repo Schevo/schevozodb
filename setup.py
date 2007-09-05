45e22d53dca81be41aeae897071053c0527d53ef
Matthew R. Scott <gldnspud@gmail.com>
2007-09-05 02:19:04 +0000

    Merge #60 to trunk.
    Schevo now uses a lightweight backend storage plugin system for handling storage of internal database structures within a Python object-oriented database.
    
    The new SchevoDurus distribution provides the new `durus` backend.  SchevoDurus is now a requirement of Schevo itself as `durus` is now the default storage engine due to its decreased startup time when opening packed databases.  It uses a Durus 3.7-schevo1, a lightly-patched version of Durus 3.7 that fixes Windows and Unicode-related bugs.  We expect to require version 3.8 when that is released.
    
    The new SchevoZodb distribution provides the new `zodb` backend, which uses ZODB3 3.7.0.
    
    The Schevo package itself provides the `schevo.store` backend, which will continue to be included with Schevo to provide backward-compatibility with existing Schevo databases and to provide for running unit tests using in-memory databases that do not write files to disk.
    
    The unit testing infrastructure in `schevo.test` now allows you to specify the backend storage engine for running tests, and also has the ability to export the standard battery of Schevo unit tests in a way that allows overriding of the backend.  This is used by SchevoDurus and SchevoZodb to run the standard Schevo tests against each of those backend.  See source:trunk/Durus/schevodurus/test/test_durus_backend.py for a usage example.
    
    Functions in `schevo.database` have been updated to support the new backend system.  You should not have to change client code for typical use cases.  See source:trunk/Schevo/schevo/database.py for more information on the API changes.
    
    `schevo.database.open` now only opens existing databases.  Use `schevo.database.create` to create new ones.  You should change client code to handle this change if your application is responsible for creating new databases and not just opening existing ones.
    
    `schevo.database.open` no longer accepts a `label` argument for specifying transient database labels.  Database labels are now persistent.  You may specify a label for a database when creating it with `schevo.database.create`. You may relabel an existing database using `schevo.label.relabel`.
    
    The new `schevo.database.copy` function copies internal structures verbatim from one database to another, and is typically used to copy an existing database using the `schevo.store` backend to a new database using the `durus` or `zodb` backend.
    
    Command line tools now accept arguments for specifying the backend and optional backend arguments.  The new `schevo db copy` command is a command-line interface to the `schevo.database.copy` function. See SchevoCommandLineTool for further information.

---
+++ setup.py
@@ -0,0 +1,76 @@
+__version__ = '3.1a1'
+
+from setuptools import setup, Extension, find_packages
+import sys, os
+import textwrap
+
+
+setup(
+    name="SchevoZodb",
+
+    version=__version__,
+
+    description="ZODB3 storage backend for Schevo",
+
+    long_description=textwrap.dedent("""
+    SchevoZodb provides integration between the ZODB3_ object database
+    for Python and the Schevo_ DBMS.
+
+    The latest development version is available in a `Subversion
+    repository <http://schevo.org/svn/trunk/Zodb#egg=SchevoZodb-dev>`__.
+
+    .. _Schevo: http://schevo.org/
+    
+    .. _ZODB3: http://www.zope.org/Products/ZODB3
+    """),
+
+    classifiers=[
+    'Development Status :: 4 - Beta',
+    'Environment :: Console',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Topic :: Database :: Database Engines/Servers',
+    'Topic :: Software Development :: Libraries :: Application Frameworks',
+    ],
+
+    keywords='database dbms',
+
+    author='Orbtech, L.L.C. and contributors',
+    author_email='schevo@googlegroups.com',
+
+    url='http://schevo.org/wiki/SchevoZodb',
+
+    license='LGPL',
+
+    platforms=['UNIX', 'Windows'],
+
+    packages=find_packages(exclude=['doc', 'tests']),
+
+    include_package_data=True,
+
+    zip_safe=False,
+
+    install_requires=[
+    'Schevo == dev, >= 3.1a1dev-r3444',
+    'ZODB3 == 3.7.0',
+    ],
+
+    tests_require=[
+    'nose >= 0.9.0',
+    ],
+    test_suite='nose.collector',
+
+    extras_require={
+    },
+
+    dependency_links = [
+    'http://schevo.org/files/thirdparty/',
+    ],
+
+    entry_points = """
+    [schevo.backend]
+    zodb = schevozodb.backend:ZodbBackend
+    """,
+    )
